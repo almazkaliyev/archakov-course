42758f779ded4bbc2ecd710d445a6206bb14c49f
Almaz Kaliyev <almaz@kaliyev.dev>
2021-02-10 01:10:11 +0600

    use immer js

---
+++ 7-redux/src/store/dummy.js
@@ -0,0 +1,46 @@
+export const steps = [
+  'Основная информация',
+  'Способ доставки',
+  'Способ оплаты',
+  'Подтверждение',
+];
+
+export const deliveryMethods = [
+  {
+    title: 'DHL',
+    period: '5-7 дней',
+    image:
+      'https://olatorera.com/wp-content/sabai/File/files/9b7d31e9e61aefda9d9932a510dd3f95.png',
+  },
+  {
+    title: 'FedEx',
+    period: '5-7 дней',
+    image:
+      'https://www.pngitem.com/pimgs/m/4-41925_fedex-logo-fedex-hd-png-download.png',
+  },
+  {
+    title: 'UPS',
+    period: '7-11 дней',
+    image:
+      'https://www.ohiobar.org/globalassets/home/member-benefits/ups/ups-logo-2014---small-rgb.jpg',
+  },
+  {
+    title: 'Почта России',
+    period: '300 дней и ночей',
+    image:
+      'https://icdn.lenta.ru/images/0000/0165/000001655648/pic_1358668888.jpg',
+  },
+];
+
+export const paymentMethods = [
+  {
+    title: 'VISA',
+    image:
+      'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0014/5140/brand.gif?itok=I-QAxA7y',
+  },
+  {
+    title: 'MasterCard',
+    image:
+      'https://cdn.vox-cdn.com/thumbor/6v89As4DbkgYpHhH_eYLbv-zkE0=/0x64:1000x731/1400x933/filters:focal(0x64:1000x731):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/50124045/mastercard_logo.0.png',
+  },
+];
--- 7-redux/src/store/reducer.js
+++ 7-redux/src/store/reducer.js
@@ -1,52 +1,15 @@
+/* eslint-disable no-param-reassign */
+import produce from 'immer';
+
 import * as ActionType from './actionTypes';
+import * as initialData from './dummy';
 
 const initialState = {
   isLoading: false,
   activeStep: 0,
-  steps: [
-    'Основная информация',
-    'Способ доставки',
-    'Способ оплаты',
-    'Подтверждение',
-  ],
-  deliveryMethods: [
-    {
-      title: 'DHL',
-      period: '5-7 дней',
-      image:
-        'https://olatorera.com/wp-content/sabai/File/files/9b7d31e9e61aefda9d9932a510dd3f95.png',
-    },
-    {
-      title: 'FedEx',
-      period: '5-7 дней',
-      image:
-        'https://www.pngitem.com/pimgs/m/4-41925_fedex-logo-fedex-hd-png-download.png',
-    },
-    {
-      title: 'UPS',
-      period: '7-11 дней',
-      image:
-        'https://www.ohiobar.org/globalassets/home/member-benefits/ups/ups-logo-2014---small-rgb.jpg',
-    },
-    {
-      title: 'Почта России',
-      period: '300 дней и ночей',
-      image:
-        'https://icdn.lenta.ru/images/0000/0165/000001655648/pic_1358668888.jpg',
-    },
-  ],
-  paymentMethods: [
-    {
-      title: 'VISA',
-      image:
-        'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0014/5140/brand.gif?itok=I-QAxA7y',
-    },
-    {
-      title: 'MasterCard',
-      image:
-        'https://cdn.vox-cdn.com/thumbor/6v89As4DbkgYpHhH_eYLbv-zkE0=/0x64:1000x731/1400x933/filters:focal(0x64:1000x731):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/50124045/mastercard_logo.0.png',
-    },
-  ],
+  steps: initialData.steps,
+  deliveryMethods: initialData.deliveryMethods,
+  paymentMethods: initialData.paymentMethods,
   formData: {
     firstName: '',
     lastName: '',
@@ -58,60 +21,44 @@ const initialState = {
   },
 };
 
-export default (state = initialState, action) => {
+export default produce((draftState, action) => {
   switch (action.type) {
     case ActionType.PREV_FORM_STEP:
-      return {
-        ...state,
-        activeStep: state.activeStep - 1,
-      };
+      draftState.activeStep -= 1;
+      break;
 
     case ActionType.NEXT_FORM_STEP:
-      return {
-        ...state,
-        activeStep: state.activeStep + 1,
-      };
+      draftState.activeStep += 1;
+      break;
 
     case ActionType.SET_CONTACTS_DATA:
-      return {
-        ...state,
-        activeStep: state.activeStep + 1,
-        formData: { ...state.formData, ...action.payload },
-      };
+      draftState.activeStep += 1;
+      draftState.formData = { ...draftState.formData, ...action.payload };
+      break;
 
     case ActionType.SET_DELIVERY_METHOD:
-      return {
-        ...state,
-        formData: { ...state.formData, deliveryMethod: action.payload },
-      };
+      draftState.formData.deliveryMethod = action.payload;
+      break;
 
     case ActionType.SET_PAYMENT_METHOD:
-      return {
-        ...state,
-        formData: { ...state.formData, paymentMethod: action.payload },
-      };
+      draftState.formData.paymentMethod = action.payload;
+      break;
 
     case ActionType.CREATE_ORDER_REQUEST:
-      return {
-        ...state,
-        isLoading: true,
-      };
+      draftState.isLoading = true;
+      break;
 
     case ActionType.CREATE_ORDER_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        activeStep: 4,
-      };
+      draftState.isLoading = false;
+      draftState.activeStep = 4;
+      break;
 
     case ActionType.CREATE_ORDER_FAILED:
-      return {
-        ...state,
-        isLoading: false,
-        activeStep: -1,
-      };
+      draftState.isLoading = false;
+      draftState.activeStep = -1;
+      break;
 
     default:
-      return state;
+      break;
   }
-};
+}, initialState);
